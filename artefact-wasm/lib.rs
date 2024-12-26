b64054f9cde6df2d3e19969e72f3cf55e03790ed
Delnegend <kiennguyen19323@gmail.com>
2024-12-27 04:51:14 +0700

    refactor(`artefact-wasm`): temporary disable params; write img to cursor before return

--- artefact-wasm/lib.rs
+++ artefact-wasm/lib.rs
@@ -1,23 +1,30 @@
-use artefact_lib::{pipeline, Config, JpegSource};
+use std::io::Cursor;
+
+use artefact_lib::{pipeline, JpegSource};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn compute(
-    weight: Vec<f32>,
-    pweight: Vec<f32>,
-    iterations: Vec<u32>,
-    separate_components: bool,
+    // weight: Vec<f32>,
+    // pweight: Vec<f32>,
+    // iterations: Vec<u32>,
+    // separate_components: bool,
     buffer: Vec<u8>,
-) -> Result<Vec<u8>, JsValue> {
-    pipeline(
-        Some(Config {
-            weight: weight.try_into().unwrap(),
-            pweight: pweight.try_into().unwrap(),
-            iterations: iterations.try_into().unwrap(),
-            separate_components,
-        }),
-        JpegSource::Buffer(buffer),
-    )
-    .map_err(|e| JsValue::from_str(&format!("{:?}", e)))
-    .map(|img| img.into_raw())
+) -> Result<Vec<u8>, String> {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    let mut cursor = Cursor::new(Vec::new());
+
+    // Some(Config {
+    //     weight: weight.try_into().unwrap(),
+    //     pweight: pweight.try_into().unwrap(),
+    //     iterations: iterations.try_into().unwrap(),
+    //     separate_components,
+    // }),
+
+    pipeline(None, JpegSource::Buffer(buffer))?
+        .write_to(&mut cursor, artefact_lib::image::ImageFormat::Png)
+        .map_err(|e| format!("Can't write image to buffer: {e:?}",))?;
+
+    Ok(cursor.into_inner())
 }
