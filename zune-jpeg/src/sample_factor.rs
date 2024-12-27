a990742c16b61e5244ac27d2ae1a524d5d0bcf9a
Delnegend <kiennguyen19323@gmail.com>
2024-12-27 16:23:35 +0700

    feat(`zune/sample_factor`): impl order and division

--- zune-jpeg/src/sample_factor.rs
+++ zune-jpeg/src/sample_factor.rs
@@ -1,6 +1,6 @@
-use core::fmt::Display;
+use core::{fmt::Display, ops::Div};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SampleFactor {
     One,
     Two,
@@ -41,6 +41,34 @@ impl Display for SampleFactor {
         match self {
             SampleFactor::One => write!(f, "1"),
             SampleFactor::Two => write!(f, "2"),
+        }
+    }
+}
+
+impl Div<SampleFactor> for SampleFactor {
+    type Output = SampleFactor;
+
+    fn div(self, rhs: SampleFactor) -> Self::Output {
+        match (self, rhs) {
+            (SampleFactor::Two, SampleFactor::One) => SampleFactor::Two,
+            _ => SampleFactor::One,
+        }
+    }
+}
+
+impl PartialOrd for SampleFactor {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for SampleFactor {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (SampleFactor::One, SampleFactor::One) => std::cmp::Ordering::Equal,
+            (SampleFactor::One, SampleFactor::Two) => std::cmp::Ordering::Less,
+            (SampleFactor::Two, SampleFactor::One) => std::cmp::Ordering::Greater,
+            (SampleFactor::Two, SampleFactor::Two) => std::cmp::Ordering::Equal,
         }
     }
 }
