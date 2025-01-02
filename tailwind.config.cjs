1361c244a77174e6f409ffc3c8f8d74df4da8633
Delnegend <kiennguyen19323@gmail.com>
2025-01-03 06:27:30 +0700

    build(`tailwind`): enable dual color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	// darkMode: ["class"],
-	// safelist: ["dark"],
+	darkMode: ["class"],
+	safelist: ["dark"],
 	prefix: "",
 
 	theme: {
