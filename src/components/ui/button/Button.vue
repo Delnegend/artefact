3db1a0f56241e0cd7795f51957399e69eb38bc52
Delnegend <kiennguyen19323@gmail.com>
2025-01-03 06:26:09 +0700

    feat: shadcn components

---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps } from "radix-vue";
+import type { HTMLAttributes } from "vue";
+
+import { cn } from "@/lib/utils";
+
+import { buttonBaseClassTw, buttonSizesTw, buttonVariantsTw } from "./index";
+
+interface Props extends PrimitiveProps {
+	variant?: keyof typeof buttonVariantsTw;
+	size?: keyof typeof buttonSizesTw;
+	class?: HTMLAttributes["class"];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	as: "button",
+	variant: "default",
+	size: "default",
+	class: "",
+});
+</script>
+
+<template>
+	<Primitive
+		:as="as"
+		:as-child="asChild"
+		:class="cn(buttonBaseClassTw, buttonVariantsTw[props.variant], buttonSizesTw[props.size], props.class)"
+	>
+		<slot />
+	</Primitive>
+</template>
