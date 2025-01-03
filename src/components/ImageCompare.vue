41fbbb2690a935b2e38f1ac312315a9f2209c07a
Delnegend <kiennguyen19323@gmail.com>
2025-01-03 19:34:26 +0700

    feat(`ImageCompare`): overlay compare mode; control buttons

--- src/components/ImageCompare.vue
+++ src/components/ImageCompare.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import { Search } from "lucide-vue-next";
+import { clamp } from "@vueuse/core";
+import { ChevronsLeftRight, Columns2, Search, SquareSplitHorizontal } from "lucide-vue-next";
 
-import { imageCompare } from "~/composables/states";
+import { imageCompareImages, imageCompareMode } from "~/composables/states";
 
 import Button from "./ui/button/Button.vue";
 
-const imageRef = ref<HTMLImageElement | null>(null);
+const containerRef = ref<HTMLDivElement | null>(null);
 
 // ===== for scaling the image =====
 
 const scale = ref(1);
-const MIN_SCALE = 1; // DO NOT PUT 0 OR SMALLER
+const MIN_SCALE = 0.1; // DO NOT PUT 0 OR SMALLER
 const MAX_SCALE = 100;
 function handleWheel(e: WheelEvent): void {
-	if (!imageRef.value) { return; }
-
 	const delta = e.deltaY > 0 ? 0.9 : 1.1;
 	scale.value = Math.min(MAX_SCALE, Math.max(MIN_SCALE, scale.value * delta));
 }
@@ -25,31 +24,80 @@ const isDragging = ref(false);
 const dragStart = ref({ x: 0, y: 0 });
 const position = ref({ x: 0, y: 0 });
 
+function doDrag(e: MouseEvent | TouchEvent): void {
+	if (!isDragging.value) { return; }
+	requestAnimationFrame(() => {
+		const pos = "touches" in e ? e.touches[0] : e;
+		position.value = {
+			x: (pos.clientX - dragStart.value.x) / scale.value,
+			y: (pos.clientY - dragStart.value.y) / scale.value,
+		};
+	});
+}
+
+function stopDrag(): void {
+	isDragging.value = false;
+
+	document.removeEventListener("mousemove", doDrag);
+	document.removeEventListener("mouseup", stopDrag);
+	document.removeEventListener("touchmove", doDrag);
+	document.removeEventListener("touchend", stopDrag);
+}
+
 function startDrag(e: MouseEvent | TouchEvent): void {
 	isDragging.value = true;
+
 	const pos = "touches" in e ? e.touches[0] : e;
 	dragStart.value = {
 		x: pos.clientX - position.value.x * scale.value,
 		y: pos.clientY - position.value.y * scale.value,
 	};
+
+	// capture mouse events globally
+	document.addEventListener("mousemove", doDrag);
+	document.addEventListener("mouseup", stopDrag);
+	document.addEventListener("touchmove", doDrag);
+	document.addEventListener("touchend", stopDrag);
 }
 
-function doDrag(e: MouseEvent | TouchEvent): void {
-	if (!isDragging.value) { return; }
-	const pos = "touches" in e ? e.touches[0] : e;
-	position.value = {
-		x: (pos.clientX - dragStart.value.x) / scale.value,
-		y: (pos.clientY - dragStart.value.y) / scale.value,
-	};
+// ===== for dragging the overlay handle =====
+
+const isDraggingSlidingHandle = ref(false);
+const slidingHandlePositionPercent = ref(50); // 0-100
+
+function doDragSlidingHandle(e: MouseEvent | TouchEvent): void {
+	if (!isDraggingSlidingHandle.value || !containerRef.value) { return; }
+
+	requestAnimationFrame(() => {
+		if (!containerRef.value) { return; }
+
+		const pos = "touches" in e ? e.touches[0] : e;
+		const rect = containerRef.value.getBoundingClientRect();
+		const percentage = ((pos.clientX - rect.left) / rect.width) * 100;
+		slidingHandlePositionPercent.value = clamp(percentage, 0, 100);
+	});
 }
 
-function stopDrag(): void {
-	isDragging.value = false;
+function stopDragSlidingHandle(): void {
+	isDraggingSlidingHandle.value = false;
+	document.removeEventListener("mousemove", doDragSlidingHandle);
+	document.removeEventListener("mouseup", stopDragSlidingHandle);
+	document.removeEventListener("touchmove", doDragSlidingHandle);
+	document.removeEventListener("touchend", stopDragSlidingHandle);
+}
+
+function startDragSlidingHandle(): void {
+	isDraggingSlidingHandle.value = true;
+
+	// capture mouse events globally
+	document.addEventListener("mousemove", doDragSlidingHandle);
+	document.addEventListener("mouseup", stopDragSlidingHandle);
+	document.addEventListener("touchmove", doDragSlidingHandle);
+	document.addEventListener("touchend", stopDragSlidingHandle);
 }
 
 // reset scale and position when image changes
-
-watch(imageCompare, () => {
+watch(imageCompareImages, () => {
 	scale.value = 1;
 	position.value = { x: 0, y: 0 };
 });
@@ -57,84 +105,116 @@ watch(imageCompare, () => {
 
 <template>
 	<div
-		v-if="imageCompare.jpegBlobUrl && imageCompare.pngBlobUrl"
-		class="grid grid-cols-2 size-full relative"
-	>
+		v-if="imageCompareImages.jpegBlobUrl && imageCompareImages.pngBlobUrl"
+		ref="containerRef"
+		class="grid size-full relative"
+		:style="{
+			display: imageCompareMode === 'side-by-side' ? 'grid' : undefined,
+			gridTemplateColumns: imageCompareMode === 'side-by-side' ? 'repeat(2, minmax(0, 1fr))' : undefined,
+		}"
+		@wheel.prevent="handleWheel">
+
+		<!-- second/png image -->
 		<div
-			ref="imageContainerRef"
-			class="cursor-grab active:cursor-grabbing relative overflow-hidden"
+			class="flex justify-center items-center overflow-hidden cursor-grab active:cursor-grabbing order-1"
+			:class="{
+				'size-full': imageCompareMode === 'side-by-side',
+				'size-full absolute top-0 left-0': imageCompareMode === 'overlay',
+			}"
+			:style="{
+				position: imageCompareMode === 'side-by-side' ? 'relative' : 'absolute',
+			}"
+
 			@mousedown.prevent="startDrag"
-			@mousemove.prevent="doDrag"
-			@mouseup.prevent="stopDrag"
-
-			@mouseleave.prevent="stopDrag"
-			@touchstart="startDrag"
-			@touchmove="doDrag"
-			@touchend="stopDrag"
-
-			@touchcancel="stopDrag"
-		>
+			@touchstart="startDrag">
 			<img
-				ref="imageRef"
-				:src="imageCompare.jpegBlobUrl"
-				class="size-full object-contain select-none origin-center"
+				:src="imageCompareImages.pngBlobUrl"
+				class="size-auto max-w-none select-none origin-center"
+				:style="{
+					transform: `scale(${scale}) translate(${position.x}px, ${position.y}px)`,
+					transition: isDragging ? 'none' : 'transform 0.1s ease-out',
+					imageRendering: 'pixelated',
+				}">
+		</div>
+
+		<!-- first/jpeg image -->
+		<div
+			class="cursor-grab overflow-hidden flex items-center active:cursor-grabbing -order-1"
+			:class="{
+				'justify-center size-full': imageCompareMode === 'side-by-side',
+				'justify-center h-full w-full absolute top-0 left-0': imageCompareMode === 'overlay',
+			}"
+			:style="{
+				clipPath: imageCompareMode === 'overlay' ? `inset(0 ${100 - slidingHandlePositionPercent}% 0 0)` : undefined,
+			}"
+
+			@mousedown.prevent="startDrag"
+			@touchstart="startDrag">
+			<img
+				:src="imageCompareImages.jpegBlobUrl"
+				class="size-auto max-w-none select-none origin-center"
 				:style="{
 					transform: `scale(${scale}) translate(${position.x}px, ${position.y}px)`,
 					transition: isDragging ? 'none' : 'transform 0.1s ease-out',
 					imageRendering: 'pixelated',
 				}"
-				@wheel.prevent="handleWheel"
-			>
+				@wheel.prevent="handleWheel">
 		</div>
+
+		<!-- compare sliding handle -->
 		<div
-			class="cursor-grab active:cursor-grabbing relative overflow-hidden"
-			@mousedown.prevent="startDrag"
-			@mousemove.prevent="doDrag"
-			@mouseup.prevent="stopDrag"
-			@mouseleave.prevent="stopDrag"
-
-			@touchstart="startDrag"
-			@touchmove="doDrag"
-			@touchend="stopDrag"
-			@touchcancel="stopDrag"
-		>
-			<img
-				ref="imageRef"
-				:src="imageCompare.pngBlobUrl"
-				class="size-full object-contain select-none origin-center"
-				:style="{
-					transform: `scale(${scale}) translate(${position.x}px, ${position.y}px)`,
-					transition: isDragging ? 'none' : 'transform 0.1s ease-out',
-					imageRendering: 'pixelated',
-				}"
-				@wheel.prevent="handleWheel"
-			>
+			v-if="imageCompareMode === 'overlay'"
+			class="absolute top-0 -translate-x-1/2 w-[2px] bg-secondary h-full flex items-center justify-center cursor-grab active:cursor-grabbing"
+			:style="{ left: `${slidingHandlePositionPercent}%` }"
+			@mousedown.prevent="startDragSlidingHandle"
+			@touchstart="startDragSlidingHandle">
+			<ChevronsLeftRight
+				:size="50"
+				color="hsl(var(--primary))"
+				class="p-2 shadow-lg bg-primary-foreground overflow-visible rounded-full" />
 		</div>
 
-		<div class="absolute bottom-4 left-5 gap-4 flex bg-transparent">
-			<Button
-				size="lg"
-				class="p-4 shadow-sm hover:shadow-md transition-all"
-				:disabled="scale === 1"
-				@click="scale = 1"
-			>
-				<Search />
-				<span class="w-7">x{{ Math.round(scale * 10) / 10 }}</span>
-			</Button>
-			<Button
-				size="lg"
-				class="shadow-sm hover:shadow-md transition-all"
-				:disabled="position.x === 0 && position.y === 0"
-				@click="position = { x: 0, y: 0 }"
-			>
-				Center
-			</Button>
+		<!-- control buttons -->
+		<div class="absolute bottom-4 w-full px-5 justify-between flex z-10">
+
+			<!-- bottom left -->
+			<div class="flex backdrop-blur bg-black/60 rounded-md">
+				<Button
+					size="lg"
+					variant="secondary"
+					class="p-4 shadow-sm hover:shadow-md transition-all"
+					:disabled="scale === 1 && position.x === 0 && position.y === 0"
+					@click="{ scale = 1; position = { x: 0, y: 0 }; }">
+					<Search />
+					<span class="w-7">x{{ Math.round(scale * 10) / 10 }}</span>
+				</Button>
+			</div>
+
+			<!-- bottom right -->
+			<div class="flex backdrop-blur bg-black/60 rounded-md gap-[1px]">
+				<Button
+					size="lg"
+					variant="secondary"
+					class="shadow-sm hover:shadow-md rounded-r-none p-4"
+					:disabled="imageCompareMode === 'side-by-side'"
+					@click="imageCompareMode = 'side-by-side'">
+					<Columns2 />
+				</Button>
+				<Button
+					size="lg"
+					variant="secondary"
+					class="shadow-sm hover:shadow-md rounded-l-none p-4"
+					:disabled="imageCompareMode === 'overlay'"
+					@click="imageCompareMode = 'overlay'">
+					<SquareSplitHorizontal />
+				</Button>
+			</div>
 		</div>
 	</div>
+
 	<div
 		v-else
-		class="size-full flex items-center justify-center text-balance text-center"
-	>
+		class="size-full flex items-center justify-center text-balance text-center">
 		<p class="text-lg text-gray-500">
 			Hit the compare button on any image.
 		</p>
