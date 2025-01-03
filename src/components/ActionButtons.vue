a951ac34aca405040f7750efb4e39465ea12cd4d
Delnegend <kiennguyen19323@gmail.com>
2025-01-04 01:17:37 +0700

    feat(`ActionButtons`): show WIP toast

--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
+import { toast } from "vue-sonner";
+
 import { cn } from "@/lib/utils";
 
 const props = defineProps<{
 	class?: string;
 }>();
+
+function WIP(): void {
+	toast.info("This feature is not yet implemented");
+}
 </script>
 
 <template>
 	<div :class="cn('gap-2 p-4 left-0 grid grid-cols-2', props.class)">
-		<Button variant="outline">
+		<Button variant="outline" @click="WIP">
 			Process all
 		</Button>
-		<Button variant="destructive">
+		<Button variant="destructive" @click="WIP">
 			Remove all
 		</Button>
-		<Button class="col-span-2">
+		<Button class="col-span-2" @click="WIP">
 			Download processed
 		</Button>
 	</div>
