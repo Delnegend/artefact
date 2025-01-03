688bbbe13951a6a5e9aac49e03e9ca0d62519311
Delnegend <kiennguyen19323@gmail.com>
2025-01-03 14:04:45 +0700

    build(`postinstall`): change from node latest to lts

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -29,7 +29,7 @@ export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
 
 # Install node and pnpm
-volta install node@23
+volta install node@lts
 volta install pnpm
 
 # change pnpm store
