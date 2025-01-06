ebfd1c3520744da26f84de8e7fc1b3a8e91b01a9
Delnegend <kiennguyen19323@gmail.com>
2025-01-06 20:03:57 +0700

    refactor(`compute_step_tv`): rename/hoist vars; shorten desc

--- artefact-lib/compute/compute_step_tv.rs
+++ artefact-lib/compute/compute_step_tv.rs
@@ -1,21 +1,6 @@
 use super::aux::Aux;
 
 /// Computes the Total Variation (TV) regularization term and its gradient
-///
-/// This function:
-/// 1. Calculates forward differences in x and y directions for each channel
-/// 2. Computes the TV norm of the gradients
-/// 3. Updates the objective function gradient
-/// 4. Stores gradient components for potential second-order TV (TGV) computation
-///
-/// # Arguments
-/// * `w` - Width of the image
-/// * `h` - Height of the image
-/// * `nchannel` - Number of color channels
-/// * `auxs` - Array of auxiliary data structures containing image data and working buffers
-///
-/// # Returns
-/// Total variation value for the objective function
 pub fn compute_step_tv(
     max_rounded_px_w: u32,
     max_rounded_px_h: u32,
@@ -24,15 +9,15 @@ pub fn compute_step_tv(
 ) -> f64 {
     let mut tv = 0.0;
 
-    for y in 0..max_rounded_px_h {
-        for x in 0..max_rounded_px_w {
+    for curr_row in 0..max_rounded_px_h {
+        for curr_row_idx in 0..max_rounded_px_w {
             compute_step_tv_inner(
                 max_rounded_px_w,
                 max_rounded_px_h,
                 nchannel,
                 auxs,
-                x,
-                y,
+                curr_row_idx,
+                curr_row,
                 &mut tv,
             );
         }
@@ -41,42 +26,38 @@ pub fn compute_step_tv(
     tv
 }
 
-fn compute_step_tv_inner(
+pub fn compute_step_tv_inner(
     max_rounded_px_w: u32,
     max_rounded_px_h: u32,
     nchannel: usize,
     auxs: &mut [Aux],
-    curr_x: u32,
-    curr_y: u32,
+    curr_row_idx: u32,
+    curr_row: u32,
     tv: &mut f64,
 ) {
     let mut g_xs = [0.0; 3];
     let mut g_ys = [0.0; 3];
 
+    let curr_px_idx = (curr_row * max_rounded_px_w + curr_row_idx) as usize;
+    let next_px_idx = curr_px_idx + 1;
+    let below_px_idx = ((curr_row + 1) * max_rounded_px_w + curr_row_idx) as usize;
+
+    let px_at_right_edge = curr_row_idx >= max_rounded_px_w - 1;
+    let px_at_bottom_edge = curr_row >= max_rounded_px_h - 1;
+
     for c in 0..nchannel {
         let aux = &mut auxs[c];
 
         // forward difference x
-        g_xs[c] = match curr_x {
-            x if x >= max_rounded_px_w - 1 => 0.0,
-            _ => {
-                let next_idx = (curr_y * max_rounded_px_w + (curr_x + 1)) as usize;
-                let prev_idx = (curr_y * max_rounded_px_w + curr_x) as usize;
-
-                aux.fdata[next_idx] - aux.fdata[prev_idx]
-            }
-        };
+        if !px_at_right_edge {
+            g_xs[c] = aux.fdata[next_px_idx] - aux.fdata[curr_px_idx];
+        }
 
         // forward difference y
-        g_ys[c] = match curr_y {
-            y if y >= max_rounded_px_h - 1 => 0.0,
-            _ => {
-                let next_idx = ((curr_y + 1) * max_rounded_px_w + curr_x) as usize;
-                let prev_idx = (curr_y * max_rounded_px_w + curr_x) as usize;
+        if !px_at_bottom_edge {
+            g_ys[c] = aux.fdata[below_px_idx] - aux.fdata[curr_px_idx];
+        }
 
-                aux.fdata[next_idx] - aux.fdata[prev_idx]
-            }
-        };
     }
 
     // norm
@@ -86,30 +67,28 @@ fn compute_step_tv_inner(
         g_norm += g_ys[c].powi(2);
     }
     g_norm = g_norm.sqrt();
+
     let alpha = 1.0 / (nchannel as f32).sqrt();
     *tv += alpha as f64 * g_norm as f64;
 
     // compute derivatives
     if g_norm != 0.0 {
         for c in 0..nchannel {
-            auxs[c].obj_gradient[(curr_y * max_rounded_px_w + curr_x) as usize] +=
-                alpha * -(g_xs[c] + g_ys[c]) / g_norm;
+            auxs[c].obj_gradient[curr_px_idx] += alpha * -(g_xs[c] + g_ys[c]) / g_norm;
 
-            if curr_x < max_rounded_px_w - 1 {
-                auxs[c].obj_gradient[(curr_y * max_rounded_px_w + (curr_x + 1)) as usize] +=
-                    alpha * g_xs[c] / g_norm;
+            if !px_at_right_edge {
+                auxs[c].obj_gradient[next_px_idx] += alpha * g_xs[c] / g_norm;
             }
 
-            if curr_y < max_rounded_px_h - 1 {
-                auxs[c].obj_gradient[((curr_y + 1) * max_rounded_px_w + curr_x) as usize] +=
-                    alpha * g_ys[c] / g_norm;
+            if !px_at_bottom_edge {
+                auxs[c].obj_gradient[below_px_idx] += alpha * g_ys[c] / g_norm;
             }
         }
     }
 
     // store for use in tv2
     for c in 0..nchannel {
-        auxs[c].pixel_diff.x[(curr_y * max_rounded_px_w + curr_x) as usize] = g_xs[c];
-        auxs[c].pixel_diff.y[(curr_y * max_rounded_px_w + curr_x) as usize] = g_ys[c];
+        auxs[c].pixel_diff.x[curr_px_idx] = g_xs[c];
+        auxs[c].pixel_diff.y[curr_px_idx] = g_ys[c];
     }
 }
