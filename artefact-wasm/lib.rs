f30f21aadc468ea56d677ff99311d5d350b414b9
Delnegend <kiennguyen19323@gmail.com>
2025-01-07 04:24:29 +0700

    refactor(`artefact-wasm/lib`): optional 1-value params

--- artefact-wasm/lib.rs
+++ artefact-wasm/lib.rs
@@ -5,27 +5,36 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn compute(
-    weight: Vec<f32>,
-    pweight: Vec<f32>,
-    iterations: Vec<u32>,
-    separate_components: bool,
+    weight: Option<f32>,
+    pweight: Option<f32>,
+    iterations: Option<u32>,
+    separate_components: Option<bool>,
     buffer: Vec<u8>,
 ) -> Result<Vec<u8>, String> {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     let mut cursor = Cursor::new(Vec::new());
+    let mut config = Config::default();
 
-    pipeline(
-        Some(Config {
-            weight: weight.try_into().unwrap(),
-            pweight: pweight.try_into().unwrap(),
-            iterations: iterations.try_into().unwrap(),
-            separate_components,
-        }),
-        JpegSource::Buffer(buffer),
-    )?
-    .write_to(&mut cursor, artefact_lib::image::ImageFormat::Png)
-    .map_err(|e| format!("Can't write image to buffer: {e:?}",))?;
+    if let Some(weight) = weight {
+        config.weight = [weight, weight, weight];
+    }
+
+    if let Some(pweight) = pweight {
+        config.pweight = [pweight, pweight, pweight];
+    }
+
+    if let Some(iterations) = iterations {
+        config.iterations = [iterations, iterations, iterations];
+    }
+
+    if let Some(separate_components) = separate_components {
+        config.separate_components = separate_components;
+    }
+
+    pipeline(Some(config), JpegSource::Buffer(buffer))?
+        .write_to(&mut cursor, artefact_lib::image::ImageFormat::Png)
+        .map_err(|e| format!("Can't write image to buffer: {e:?}",))?;
 
     Ok(cursor.into_inner())
 }
