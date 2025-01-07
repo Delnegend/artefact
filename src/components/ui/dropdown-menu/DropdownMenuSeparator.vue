9fe336e9bf934b9d56c3c29b9f85b620d9ad3695
Delnegend <kiennguyen19323@gmail.com>
2025-01-07 18:12:50 +0700

    feat(`ui`): dropdown menu

---
+++ src/components/ui/dropdown-menu/DropdownMenuSeparator.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+	DropdownMenuSeparator,
+	type DropdownMenuSeparatorProps,
+} from "radix-vue";
+import { computed, type HTMLAttributes } from "vue";
+
+import { cn } from "@/lib/utils";
+
+const props = defineProps<DropdownMenuSeparatorProps & {
+	class?: HTMLAttributes["class"];
+}>();
+
+const delegatedProps = computed(() => {
+	// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unsafe-assignment
+	const { class: _, ...delegated } = props;
+
+	return delegated;
+});
+</script>
+
+<template>
+	<DropdownMenuSeparator v-bind="delegatedProps" :class="cn('-mx-1 my-1 h-px bg-muted', props.class)" />
+</template>
