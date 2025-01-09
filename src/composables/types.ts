2896f3c593b027b54d5ec672353ea88ed336e7ec
Delnegend <kiennguyen19323@gmail.com>
2025-01-10 03:01:33 +0700

    feat(`types.ts`): `OutputImgFormat`

--- src/composables/types.ts
+++ src/composables/types.ts
@@ -1,3 +1,14 @@
+export enum OutputImgFormat {
+	// eslint-disable-next-line no-unused-vars
+	PNG = "png",
+	// eslint-disable-next-line no-unused-vars
+	WEBP = "webp",
+	// eslint-disable-next-line no-unused-vars
+	TIF = "tif",
+	// eslint-disable-next-line no-unused-vars
+	BMP = "bmp",
+}
+
 export interface ImageItemForDB {
 	jpegFileHash: string;
 	jpegFileName: string;
