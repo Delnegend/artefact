7b5ec84fdaceefffba219a9a5765ee47e13fa357
Delnegend <kiennguyen19323@gmail.com>
2025-01-10 03:15:23 +0700

    feat(`ImageItem`): use `pinia` stores; add re-process button; add img format label
    - lint
    - `props.class`
    - get time measure from worker

--- src/components/ImageItem.vue
+++ src/components/ImageItem.vue
@@ -3,13 +3,24 @@ import { LoaderCircle } from "lucide-vue-next";
 import { h, ref } from "vue";
 import { toast } from "vue-sonner";
 
+import Badge from "~/components/ui/badge/Badge.vue";
 import { humanReadableSize } from "~/composables/human-readable-size";
-import { db, imageCompareImages, imageDisplayList } from "~/composables/states";
-import type { ImageItemForDisplay } from "~/composables/types";
+import { db, imageDisplayList, useImageCompareStore, useProcessingConfig } from "~/composables/states";
+import type { ImageItemForDisplay, OutputImgFormat, WorkerInput, WorkerOutput } from "~/composables/types";
+import { cn } from "~/lib/utils";
 
-const props = defineProps<{ jpegFileHash: string; info: ImageItemForDisplay }>();
+const props = defineProps<{
+	jpegFileHash: string;
+	info: ImageItemForDisplay;
+	class?: string;
+}>();
 
-const pngBlobUrl = ref<string | undefined>(props.info.pngBlobUrl);
+const imageCompareStore = useImageCompareStore();
+const processingConfig = useProcessingConfig();
+
+const outputImgBlobUrl = ref<string | undefined>(props.info.outputImgBlobUrl);
+const outputImgFormat = ref<OutputImgFormat | undefined>(props.info.outputImgFormat);
+
 const isProcessing = ref(false);
 
 const worker = new Worker(
@@ -18,7 +29,7 @@ const worker = new Worker(
 );
 
 worker.onmessage = (e): void => {
-	const { blobUrl, error } = e.data as { blobUrl?: string; error?: string };
+	const { blobUrl, error, timeTakenInMs, outputFormat } = e.data as WorkerOutput;
 
 	if (error) {
 		toast.error("Error", {
@@ -28,7 +39,7 @@ worker.onmessage = (e): void => {
 		return;
 	}
 
-	if (!blobUrl) {
+	if (!blobUrl || timeTakenInMs === undefined) {
 		toast.error("Error", {
 			description: "The worker doesn't return the image nor any error.",
 		});
@@ -39,10 +50,12 @@ worker.onmessage = (e): void => {
 	toast.info("Success", {
 		description: h("div", [
 			h("code", props.info.name),
-			" is processed successfully.",
+			" done in ",
+			h("code", `${(timeTakenInMs / 1000).toFixed(2)}s`),
 		]),
 	});
-	pngBlobUrl.value = blobUrl;
+	outputImgBlobUrl.value = blobUrl;
+	outputImgFormat.value = outputFormat;
 };
 
 worker.onerror = (e): void => {
@@ -53,7 +66,7 @@ worker.onerror = (e): void => {
 };
 
 function process(): void {
-	if (pngBlobUrl.value) {
+	if (outputImgBlobUrl.value) {
 		toast.error("Error", {
 			description: "The image is already processed.",
 		});
@@ -68,11 +81,22 @@ function process(): void {
 	}
 
 	isProcessing.value = true;
-	worker.postMessage(props.jpegFileHash);
+
+	const req: WorkerInput = {
+		jpegFileHash: props.jpegFileHash,
+		config: {
+			outputFormat: processingConfig.outputFormat,
+			iterations: processingConfig.iterations,
+			weight: processingConfig.weight,
+			pWeight: processingConfig.pWeight,
+			separateComponents: processingConfig.separateComponents,
+		},
+	};
+	worker.postMessage(req);
 }
 
 function download(): void {
-	if (!pngBlobUrl.value) {
+	if (!outputImgBlobUrl.value) {
 		toast.error("Error", {
 			description: "The image is not processed yet.",
 		});
@@ -82,51 +106,53 @@ function download(): void {
 	const a = document.createElement("a");
 	a.style.display = "none";
 	a.download = `${props.info.name.split(".").slice(0, -1)
-		.join(".")}.png`;
-	a.href = pngBlobUrl.value;
+		.join(".")}.${outputImgFormat.value}`;
+	a.href = outputImgBlobUrl.value;
 	document.body.appendChild(a);
 	a.click();
 	document.body.removeChild(a);
 }
 
 function compare(): void {
-	if (!pngBlobUrl.value) {
+	if (!outputImgBlobUrl.value) {
 		toast.error("Error", {
 			description: "The image is not processed yet.",
 		});
 		return;
 	}
 
-	imageCompareImages.value = {
-		jpegBlobUrl: props.info.jpegBlobUrl,
-		pngBlobUrl: pngBlobUrl.value,
-	};
+	imageCompareStore.jpegBlobUrl = props.info.jpegBlobUrl;
+	imageCompareStore.outputImgBlobUrl = outputImgBlobUrl.value;
 }
 
 function remove(): void {
 	worker.terminate();
 	void db.delete("files", props.jpegFileHash);
 	imageDisplayList.value.delete(props.jpegFileHash);
-	if (imageCompareImages.value.jpegBlobUrl === props.info.jpegBlobUrl) {
-		imageCompareImages.value = { jpegBlobUrl: undefined, pngBlobUrl: undefined };
+	if (imageCompareStore.jpegBlobUrl === props.info.jpegBlobUrl) {
+		imageCompareStore.jpegBlobUrl = undefined;
+		imageCompareStore.outputImgBlobUrl = undefined;
 	}
+}
+
+function reprocess(): void {
+	toast.info("This feature is not implemented yet.");
 }
 </script>
 
 <template>
-	<div class="px-4 flex flex-col gap-3">
-		<div class="flex flex-row h-fit gap-4 items-center">
+	<div :class="cn('px-4 flex flex-col gap-3', props.class)">
+		<div class="grid grid-cols-[auto,1fr] gap-4 items-center">
 			<img
 				:src="info.jpegBlobUrl"
 				class="rounded-md size-16 object-cover aspect-square">
 
-			<div class="flex flex-col h-full w-full justify-between overflow-hidden">
+			<div class="flex flex-col size-full justify-between overflow-hidden">
 				<div
-				class="font-medium text-transparent bg-clip-text line-clamp-1 text-ellipsis overflow-hidden"
-				:style="{
-					backgroundImage: 'linear-gradient(90deg, hsl(var(--primary)) 70%, transparent 100%)'
-				}"
-				>
+					class="font-medium text-transparent bg-clip-text line-clamp-1 text-ellipsis overflow-hidden"
+					:style="{
+						backgroundImage: 'linear-gradient(90deg, hsl(var(--primary)) 70%, transparent 100%)'
+					}">
 					{{ info.name }}
 				</div>
 
@@ -143,33 +169,44 @@ function remove(): void {
 			</div>
 		</div>
 
-		<div class="grid grid-cols-3 gap-2">
+		<div class="grid grid-cols-2 grid-rows-2 gap-2">
+			<div class="grid">
+				<Button
+					v-if="!outputImgBlobUrl"
+					:disabled="isProcessing"
+					@click="process">
+					<span v-if="!isProcessing">Process</span>
+					<span v-else class="animate-spin">
+						<LoaderCircle />
+					</span>
+				</Button>
+				<Button
+					v-if="outputImgBlobUrl"
+					:disabled="!outputImgBlobUrl"
+					class="relative"
+					@click="download">
+					Download
+					<Badge class="absolute -bottom-3 scale-90 backdrop-blur-sm bg-primary-foreground/90" variant="outline">{{ outputImgFormat }}</Badge>
+				</Button>
+			</div>
+
 			<Button
-				v-if="!pngBlobUrl"
-				:disabled="isProcessing"
-				@click="process">
-				<span v-if="!isProcessing">Process</span>
-				<span v-else class="animate-spin">
-					<LoaderCircle />
-				</span>
-			</Button>
-			<Button
-				v-if="pngBlobUrl"
 				variant="outline"
-				:disabled="!pngBlobUrl"
-				@click="download">
-				Download
+				:disabled="!outputImgBlobUrl"
+				@click="reprocess">
+				Re-process
 			</Button>
 
 			<Button
 				variant="secondary"
-				:disabled="!pngBlobUrl"
+				:disabled="!outputImgBlobUrl"
 				@click="compare">
 				Compare
 			</Button>
 
 			<Button
-				variant="destructive"
+				variant="outline"
+				class="text-red-500 hover:text-red-600"
 				@click="remove">
 				<div>Remove</div>
 			</Button>
