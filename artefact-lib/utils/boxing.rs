c0cd8f505346aca5a4db613a2ccb8512716896d1
Delnegend <kiennguyen19323@gmail.com>
2025-01-09 19:18:18 +0700

    refactor(`boxing`): add scalar version; toggle w/ feature flag

--- artefact-lib/utils/boxing.rs
+++ artefact-lib/utils/boxing.rs
@@ -1,4 +1,8 @@
+#[cfg(feature = "simd")]
 use wide::f32x8;
+
+#[cfg(feature = "simd")]
+use crate::compute::f32x8;
 
 /// Convert from 8x8 block to 64x1 block
 pub fn unboxing(
@@ -17,12 +21,23 @@ pub fn unboxing(
 
     for block_y in 0..block_h {
         for block_x in 0..block_w {
+            #[cfg(feature = "simd")]
             for in_y in 0..8 {
-                let result = f32x8::from(&input[index..index + 8]);
+                let result = f32x8!(&input[index..index + 8]);
 
                 let row_start = ((block_y * 8 + in_y) * rounded_px_w + (block_x * 8)) as usize;
                 output[row_start..row_start + 8].copy_from_slice(result.as_array_ref());
                 index += 8;
+            }
+
+            #[cfg(not(feature = "simd"))]
+            for in_y in 0..8 {
+                for in_x in 0..8 {
+                    output[((block_y * 8 + in_y) * rounded_px_w + (block_x * 8 + in_x)) as usize] =
+                        input[index];
+
+                    index += 1;
+                }
             }
         }
     }
@@ -45,12 +60,22 @@ pub fn boxing(
 
     for block_y in 0..block_h {
         for block_x in 0..block_w {
+            #[cfg(feature = "simd")]
             for in_y in 0..8 {
                 let row_start = ((block_y * 8 + in_y) * rounded_px_w + (block_x * 8)) as usize;
-                let result = f32x8::from(&input[row_start..row_start + 8]);
+                let result = f32x8!(&input[row_start..row_start + 8]);
 
                 output[index..index + 8].copy_from_slice(result.as_array_ref());
                 index += 8;
+            }
+
+            #[cfg(not(feature = "simd"))]
+            for in_y in 0..8 {
+                for in_x in 0..8 {
+                    output[index] = input
+                        [((block_y * 8 + in_y) * rounded_px_w + (block_x * 8 + in_x)) as usize];
+                    index += 1;
+                }
             }
         }
     }
