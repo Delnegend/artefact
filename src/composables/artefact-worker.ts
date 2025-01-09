54eb0ddeca92a4e62b40b06996e747e23742fd9e
Delnegend <kiennguyen19323@gmail.com>
2025-01-10 03:06:07 +0700

    feat(`artefact-worker`): use user-specified params

--- src/composables/artefact-worker.ts
+++ src/composables/artefact-worker.ts
@@ -1,23 +1,35 @@
-import init, { compute } from "./artefact-wasm";
+import init, { compute, type OutputFormat } from "./artefact-wasm";
 import { db } from "./states";
-import type { ImageItemForDB } from "./types";
+import { type ImageItemForDB, OutputImgFormat, type WorkerInput, type WorkerOutput } from "./types";
 
-type ImageFileHash = string;
+self.onmessage = async (event: MessageEvent<WorkerInput>): Promise<void> => {
+	const { jpegFileHash, config } = event.data;
 
-self.onmessage = async (event: MessageEvent<ImageFileHash>): Promise<void> => {
 	// eslint-disable-next-line no-unused-vars
-	const [_, imageInDB] = await Promise.all([init(), db.get("files", event.data) as Promise<ImageItemForDB | undefined>]);
+	const [_, imageInDB] = await Promise.all([init(), db.get("files", jpegFileHash) as Promise<ImageItemForDB | undefined>]);
 	if (!imageInDB) {
 		self.postMessage({ error: "Image not found in the database." });
 		return;
 	}
 
-	// get png data
-	let pngDataArray: Uint8Array;
+	const format = ((): OutputFormat => {
+		switch (config.outputFormat) {
+			case OutputImgFormat.PNG: return 0;
+			case OutputImgFormat.WEBP: return 1;
+			case OutputImgFormat.TIF: return 2;
+			case OutputImgFormat.BMP: return 3;
+			default: return 0;
+		}
+	})();
+
+	let outputImgDataArray: Uint8Array;
+	let timer = new Date().getTime();
 	try {
-		pngDataArray = compute(new Uint8Array(imageInDB.jpegArrayBuffer));
+		outputImgDataArray = compute(new Uint8Array(imageInDB.jpegArrayBuffer), format, config.weight, config.pWeight, config.iterations, config.separateComponents);
+		timer = new Date().getTime() - timer;
 	} catch (e) {
-		self.postMessage({ error: e });
+		const resp: WorkerOutput = { error: `${e}` };
+		self.postMessage(resp);
 		return;
 	}
 
@@ -25,18 +37,25 @@ self.onmessage = async (event: MessageEvent<ImageFileHash>): Promise<void> => {
 	void (async (): Promise<void> => {
 		const tx = db.transaction("files", "readwrite");
 		const store = tx.objectStore("files");
-		const item = await store.get(event.data) as ImageItemForDB | undefined;
+		const item = await store.get(jpegFileHash) as ImageItemForDB | undefined;
 		if (item) {
-			item.pngArrayBuffer = pngDataArray.buffer as ArrayBuffer;
+			item.outputImgArrayBuffer = outputImgDataArray.buffer as ArrayBuffer;
+			item.outputImgFormat = config.outputFormat;
 			await store.put(item);
 		}
 		await tx.done;
 	})();
 
 	// create blob
-	const blob = new Blob([pngDataArray], { type: "image/png" });
+	const blob = new Blob([outputImgDataArray], {
+		type: `image/${config.outputFormat}`,
+	});
 	const blobUrl = URL.createObjectURL(blob);
 
 	// respond
-	self.postMessage({ blobUrl });
+	const resp: WorkerOutput = {
+		blobUrl, timeTakenInMs: timer,
+		outputFormat: config.outputFormat,
+	};
+	self.postMessage(resp);
 };
