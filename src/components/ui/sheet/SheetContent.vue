5fe5090e0dbba95a891160c59e202ef5e2da403d
Delnegend <kiennguyen19323@gmail.com>
2025-01-10 03:06:34 +0700

    feat(`SheetContent.vue`): add `description`; rm double space in class

--- src/components/ui/sheet/SheetContent.vue
+++ src/components/ui/sheet/SheetContent.vue
@@ -22,6 +22,7 @@ defineOptions({
 const props = defineProps<DialogContentProps & {
 	class?: HTMLAttributes["class"];
 	side?: keyof typeof sheetSide;
+	description: string;
 }>();
 
 const emits = defineEmits<DialogContentEmits>();
@@ -39,11 +40,12 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 <template>
 	<DialogPortal>
 		<DialogOverlay
-			class="fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+			class="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
 		/>
 		<DialogContent
 			:class="cn(sheetBaseClass, sheetSide[props.side ?? 'left'], props.class)"
 			v-bind="{ ...forwarded, ...$attrs }"
+			:aria-describedby="props.description"
 		>
 			<slot />
 
