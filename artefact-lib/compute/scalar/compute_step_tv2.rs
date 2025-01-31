742c340992d62e965336d7bfd821927f7a319130
Delnegend <kiennguyen19323@gmail.com>
2025-02-01 03:53:27 +0700

    refactor(`scalar/compute_step_tv2`): clippy

--- artefact-lib/compute/scalar/compute_step_tv2.rs
+++ artefact-lib/compute/scalar/compute_step_tv2.rs
@@ -1,4 +1,4 @@
-use crate::compute::aux::Aux;
+use crate::{compute::aux::Aux, utils::macros::mul_add};
 
 /// Computes the Total Generalized Variation (TGV) regularization term and its gradient
 pub fn compute_step_tv2(
@@ -45,50 +45,28 @@ fn compute_step_tv2_inner(
 
     for c in 0..nchannel {
         let aux = &mut auxs[c];
+        #[allow(clippy::useless_let_if_seq)]
+        let mut g_xy = 0.0;
+        #[allow(clippy::useless_let_if_seq)]
+        let mut g_yx = 0.0;
 
         // backward difference x
-        g_xxs[c] = match curr_x {
-            ..=0 => 0.0,
-            _ => {
-                let curr_idx = (curr_y * max_rounded_px_w + curr_x) as usize;
-                let prev_idx = (curr_y * max_rounded_px_w + (curr_x - 1)) as usize;
+        if curr_x != 0 {
+            let a = (curr_y * max_rounded_px_w + curr_x) as usize;
+            let b = (curr_y * max_rounded_px_w + (curr_x - 1)) as usize;
 
-                aux.pixel_diff.x[curr_idx] - aux.pixel_diff.x[prev_idx]
-            }
-        };
-
-        // backward difference x
-        let g_yx = match curr_x {
-            ..=0 => 0.0,
-            _ => {
-                let curr_idx = (curr_y * max_rounded_px_w + curr_x) as usize;
-                let prev_idx = (curr_y * max_rounded_px_w + (curr_x - 1)) as usize;
-
-                aux.pixel_diff.y[curr_idx] - aux.pixel_diff.y[prev_idx]
-            }
-        };
+            g_xxs[c] = aux.pixel_diff.x[a] - aux.pixel_diff.x[b];
+            g_yx = aux.pixel_diff.y[a] - aux.pixel_diff.y[b];
+        }
 
         // backward difference y
-        let g_xy = match curr_y {
-            ..=0 => 0.0,
-            _ => {
-                let curr_idx = (curr_y * max_rounded_px_w + curr_x) as usize;
-                let prev_idx = ((curr_y - 1) * max_rounded_px_w + curr_x) as usize;
+        if curr_y != 0 {
+            let a = (curr_y * max_rounded_px_w + curr_x) as usize;
+            let b = ((curr_y - 1) * max_rounded_px_w + curr_x) as usize;
 
-                aux.pixel_diff.x[curr_idx] - aux.pixel_diff.x[prev_idx]
-            }
-        };
-
-        // backward difference y
-        g_yys[c] = match curr_y {
-            ..=0 => 0.0,
-            _ => {
-                let curr_idx = (curr_y * max_rounded_px_w + curr_x) as usize;
-                let prev_idx = ((curr_y - 1) * max_rounded_px_w + curr_x) as usize;
-
-                aux.pixel_diff.y[curr_idx] - aux.pixel_diff.y[prev_idx]
-            }
-        };
+            g_yys[c] = aux.pixel_diff.y[a] - aux.pixel_diff.y[b];
+            g_xy = aux.pixel_diff.x[a] - aux.pixel_diff.x[b];
+        }
 
         // symmetrize
         g_xy_syms[c] = (g_xy + g_yx) / 2.0;
@@ -98,14 +76,18 @@ fn compute_step_tv2_inner(
     let g2_norm = {
         let mut g2_norm = 0.0;
         for c in 0..nchannel {
-            g2_norm += g_xxs[c].powi(2) + 2.0 * g_xy_syms[c].powi(2) + g_yys[c].powi(2);
+            g2_norm += mul_add!(
+                g_yys[c],
+                g_yys[c],
+                mul_add!(2.0_f32, g_xy_syms[c].powi(2), g_xxs[c].powi(2))
+            );
         }
         g2_norm.sqrt()
     };
 
     let mut alpha = alpha;
     alpha /= (nchannel as f32).sqrt();
-    *tv2 += (alpha * g2_norm) as f64; // objective function
+    *tv2 += f64::from(alpha * g2_norm); // objective function
 
     if g2_norm == 0.0 {
         return;
@@ -119,7 +101,7 @@ fn compute_step_tv2_inner(
         let aux = &mut auxs[c];
 
         aux.obj_gradient[(curr_y * max_rounded_px_w + curr_x) as usize] +=
-            alpha * (-(2.0 * g_xx + 2.0 * g_xy_sym + 2.0 * g_yy) / g2_norm);
+            alpha * (-(mul_add!(2.0_f32, g_yy, mul_add!(2.0_f32, g_xx, 2.0 * g_xy_sym))) / g2_norm);
 
         if curr_x > 0 {
             aux.obj_gradient[(curr_y * max_rounded_px_w + (curr_x - 1)) as usize] +=
