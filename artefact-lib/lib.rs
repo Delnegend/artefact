8d5aecf3daf639165781a7da0dd9e44c225fa58a
Delnegend <kiennguyen19323@gmail.com>
2025-02-02 14:03:40 +0700

    refactor(`artefact-lib/lib`): default to 50 iterations

--- artefact-lib/lib.rs
+++ artefact-lib/lib.rs
@@ -70,7 +70,7 @@ impl Default for Artefact {
         Self {
             weight: ValueCollection::ForAll(0.3),
             pweight: ValueCollection::ForAll(0.001),
-            iterations: ValueCollection::ForAll(100),
+            iterations: ValueCollection::ForAll(50),
             separate_components: false,
             benchmark: false,
             source: None,
