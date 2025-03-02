3f698c760efec621545f7f8d3dccd02102864618
Delnegend <kiennguyen19323@gmail.com>
2025-03-03 00:16:15 +0700

    build: upgrade deps; new eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,27 +8,12 @@ export default [
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	...hagemanto({
 		enableJsx: false,
-		extraFileExtensions: [".vue"],
+		pluginVue: pluginVue.configs["flat/recommended"],
 	}),
-	...pluginVue.configs["flat/recommended"],
-
 	{
 		rules: {
 			"no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
 			"tailwindcss/no-custom-classname": "off",
-			"vue/html-indent": ["error", "tab"],
-			"vue/multi-word-component-names": "off",
-			"vue/html-closing-bracket-newline": [
-				"error",
-				{
-					singleline: "never",
-					multiline: "never",
-					selfClosingTag: {
-						singleline: "never",
-						multiline: "never",
-					},
-				},
-			],
 		},
 	},
 ];
