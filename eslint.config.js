67bfca8505910e25bec0b9c776a33f831316ddf6
Delnegend <kiennguyen19323@gmail.com>
2025-05-06 11:16:25 +0700

    chore(`eslint.config`): globals config; disable no unused vars; allow no-unsafe-assignment in dropdown menu

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,39 @@
 import hagemanto from "eslint-plugin-hagemanto";
 import pluginVue from "eslint-plugin-vue";
 
+/** @type { import("eslint").Linter.Config[] } */
 export default [
-	{ files: ["**/*.{ts,vue}"] },
-	{ ignores: ["artefact-wasm/**/*", "src/utils/artefact-wasm/**/*", "node_modules/**/*", "src/dev-dist/**/*", ".nuxt/**/*", "*.config.*"] },
-
-	...hagemanto({
+	{
+		files: ["**/*.{ts,vue}"],
+	}, {
+		ignores: [
+			"artefact-wasm/**/*",
+			"src/utils/artefact-wasm/**/*",
+			"node_modules/**/*",
+			"src/dev-dist/**/*",
+			".nuxt/**/*",
+		],
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	}, ...hagemanto({
 		enableJsx: false,
 		vueConfig: pluginVue.configs["flat/recommended"],
-	}),
+	}), {
+		languageOptions: {
+			globals: {
+				document: true,
+				window: true,
+				module: true,
+				require: true,
+			},
+		},
+	}, {
+		rules: {
+			"no-unused-vars": 0,
+		},
+	}, {
+		files: ["src/components/ui/dropdown-menu/*"],
+		rules: {
+			"@typescript-eslint/no-unsafe-assignment": 0,
+		},
+	},
 ];
