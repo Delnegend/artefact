63342fc3ebd08ce5a1aa1a0144f86529c8ca2404
Delnegend <kiennguyen19323@gmail.com>
2025-12-25 13:06:05 +0700

    build(`postinstall`): upgrade tooling

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -7,10 +7,8 @@ echo 'export PATH="/usr/local/cargo/bin:$PATH"' >> ~/.bashrc
 # mold
 ver="2.40.4"
 curl -L -o /tmp/mold.tar.gz https://github.com/rui314/mold/releases/download/v$ver/mold-$ver-x86_64-linux.tar.gz
-checksum=$(openssl dgst -sha3-512 /tmp/mold.tar.gz | awk '{print $2}')
-expected="1faf14077e25608993985b2e1e307ce2f36dc5653baedd6b4460775b1058d4120f7e225206b96cee861e0267b06aeb9dfed010e8dccedaacfb6f2672df7b7490"
-if [ ! "$checksum" = "$expected" ]; then
-    echo "mold tarball checksum failed\nexpected: $expected\ngot: $checksum"
+if [ "$(sha256sum /tmp/mold.tar.gz | awk '{print $1}')" != "4c999e19ffa31afa5aa429c679b665d5e2ca5a6b6832ad4b79668e8dcf3d8ec1" ]; then
+    echo "mold tarball checksum failed"
 else
     sudo rm -rf /usr/local/cargo/mold*
     sudo tar -xf /tmp/mold.tar.gz -C /usr/local/cargo mold-$ver-x86_64-linux
@@ -22,12 +20,10 @@ rm -f /usr/local/cargo/config.toml
 printf "[target.x86_64-unknown-linux-gnu]\nlinker = \"clang\"\nrustflags = [\"-C\", \"link-arg=-fuse-ld=/usr/local/cargo/mold-$ver-x86_64-linux/bin/mold\"]" > /usr/local/cargo/config.toml
 
 # just
-ver="1.43.0"
+ver="1.45.0"
 curl -L -o /tmp/just.tar.gz https://github.com/casey/just/releases/download/$ver/just-$ver-x86_64-unknown-linux-musl.tar.gz
-checksum=$(openssl dgst -sha3-512 /tmp/just.tar.gz | awk '{print $2}')
-expected="84d1ce2248381502507a5b6a67b3b6bc80fd98e52f8604f2628a087405272b10b371d235b11051f49176aa9fb4c17f6c7cb1d96e05f755a799b2ef05b6d3e3e8"
-if [ ! "$checksum" = "$expected" ]; then
-    echo "just tarball checksum failed\nexpected: $expected\ngot: $checksum"
+if [ ! "$(sha256sum /tmp/just.tar.gz | awk '{print $1}')" != "dc3f958aaf8c6506dd90426e9b03f86dd15e74a6467ee0e54929f750af3d9e49" ]; then
+    echo "just tarball checksum failed"
 else
     sudo rm -rf /usr/local/bin/just
     sudo tar -xf /tmp/just.tar.gz -C /usr/local/bin just
@@ -38,14 +34,11 @@ echo 'eval "$(just --completions bash)"' >> ~/.bashrc
 echo 'complete -F _just j' >> ~/.bashrc
 
 # fzf
-ver="0.66.0"
+ver="0.67.0"
 curl -L -o /tmp/fzf.tar.gz https://github.com/junegunn/fzf/releases/download/v$ver/fzf-$ver-linux_amd64.tar.gz
-checksum=$(openssl dgst -sha3-512 /tmp/fzf.tar.gz | awk '{print $2}')
-expected="1710205b6f924c78ebfc6b43e1697e4cf4ba168d7970196f23effb4f125e956a76a07ae8a26dfcd1a4a5b26435b2670bb840b7d1c4ea92befef09789d17068b0"
-if [ ! "$checksum" = "$expected" ]; then
-    echo "fzf tarball checksum failed\nexpected: $expected\ngot: $checksum"
+if [ "$(sha256sum /tmp/fzf.tar.gz | awk '{print $1}')" != "4be08018ca37b32518c608741933ea335a406de3558242b60619e98f25be2be1" ]; then
+    echo "fzf tarball checksum failed"
 else
-    sudo rm -rf /usr/local/bin/fzf
     sudo tar -xf /tmp/fzf.tar.gz -C /usr/local/bin fzf
 fi
 rm -f /tmp/fzf.tar.gz
