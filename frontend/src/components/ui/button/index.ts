a193bc74358f1b84f56b80252bfe018b15916f71
Delnegend <kiennguyen19323@gmail.com>
2025-12-26 14:33:14 +0700

    fix(`frontend/button`): rm nowrap

--- frontend/src/components/ui/button/index.ts
+++ frontend/src/components/ui/button/index.ts
@@ -1,7 +1,7 @@
 export { default as Button } from './Button.vue'
 
 export const buttonBaseClassTw =
-	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 select-none'
+	'inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 select-none'
 
 export const buttonVariantsTw = {
 	default: 'bg-primary text-primary-foreground shadow-sm hover:bg-primary/90',
